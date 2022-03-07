9ce2bd3fa8a1094f8deb799a1f4f68d7db704237
David Raznick <kindly@gmail.com>
2022-03-07 20:48:53 +0000

    lower sqlite memrory

--- src/lib.rs
+++ src/lib.rs
@@ -1498,7 +1498,6 @@ impl FlatFiles {
         conn.execute_batch(
             "PRAGMA journal_mode = OFF;
              PRAGMA synchronous = 0;
-             PRAGMA cache_size = 1000000;
              PRAGMA locking_mode = EXCLUSIVE;
              PRAGMA temp_store = MEMORY;",
         )
